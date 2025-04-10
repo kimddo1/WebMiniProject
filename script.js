ba78960bdacdbeafbbee624d714e23b7a5aa7eba
seoncho1 <seoncho07@gmail.com>
2025-04-10 17:25:50 +0900

    reload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* sidebar */
+
+/* global bootstrap: false */
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
